7f27728c7601b880bf8610eb21117ce7a93cdf04
DevLuv_JinHyung <wlsgud16@gmail.com>
2022-05-06 23:41:19 +0900

    2022/05/06 Update) TankBattalion
    Project of Game Engine Programming

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Brick : MonoBehaviour
+{
+    Tilemap tilemap;
+    AudioSource audio;
+
+    private Vector3Int breakPos;
+
+    public AudioClip exploSound;
+
+    private void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("PBullet") || collision.CompareTag("EBullet"))
+        {
+            PlaySound("Explo");
+            breakPos = tilemap.WorldToCell(collision.transform.position);
+            if (tilemap.GetTile(breakPos) == null)
+            {
+                if (tilemap.GetTile(new Vector3Int(breakPos.x + 1, breakPos.y, breakPos.z)) != null)
+                {
+                    breakPos.x += 1;
+                }
+                if (tilemap.GetTile(new Vector3Int(breakPos.x - 1, breakPos.y, breakPos.z)) != null)
+                {
+                    breakPos.x -= 1;
+                }
+                if (tilemap.GetTile(new Vector3Int(breakPos.x, breakPos.y + 1, breakPos.z)) != null)
+                {
+                    breakPos.y += 1;
+                }
+                if (tilemap.GetTile(new Vector3Int(breakPos.x, breakPos.y - 1, breakPos.z)) != null)
+                {
+                    breakPos.y -= 1;
+                }
+            }
+            tilemap.SetTile(breakPos, null);
+        }
+    }
+
+    private void PlaySound(string name)
+    {
+        switch(name)
+        {
+            case "Explo":
+                audio.clip = exploSound;
+                break;
+        }
+
+        audio.Play();
+    }
+}
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject brickEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Border":
+                Destroy(this.gameObject);
+                break;
+            case "PBullet":
+                Destroy(this.gameObject);
+                break;
+            case "EBullet":
+                Destroy(this.gameObject);
+                break;
+            case "Flag":
+                Destroy(this.gameObject);
+                break;
+            case "Brick":
+                GameObject effect = Instantiate(brickEffect, transform.position, transform.rotation);
+                Destroy(effect, 0.5f);
+                Destroy(this.gameObject);
+                break;
+
+        }
+    }
+}
